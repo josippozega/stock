cb7cb2dd846ad23062c33f6f490da5c98d2c1b7b
josippozega <96910194+josippozega@users.noreply.github.com>
2022-01-08 04:23:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 from datetime import date
 
 import yfinance as yf
-#from fbprophet import Prophet
-#from fbprophet.plot import plot_plotly
+from fbprophet import Prophet
+from fbprophet.plot import plot_plotly
 from plotly import graph_objs as go
 
 START = "2015-01-01"
@@ -46,19 +46,19 @@ plot_raw_data()
 df_train = data[['Date','Close']]
 df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
 
-#m = Prophet()
-#m.fit(df_train)
-#future = m.make_future_dataframe(periods=period)
-#forecast = m.predict(future)
+m = Prophet()
+m.fit(df_train)
+future = m.make_future_dataframe(periods=period)
+forecast = m.predict(future)
 
 # Show and plot forecast
 st.subheader('Forecast data')
-#st.write(forecast.tail())
+st.write(forecast.tail())
 
 st.write(f'Forecast plot for {n_years} years')
-#fig1 = plot_plotly(m, forecast)
-#st.plotly_chart(fig1)
+fig1 = plot_plotly(m, forecast)
+st.plotly_chart(fig1)
 
 st.write("Forecast components")
-#fig2 = m.plot_components(forecast)
-#st.write(fig2)
+fig2 = m.plot_components(forecast)
+st.write(fig2)
